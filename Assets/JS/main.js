3d28682ce46e1fd8a172190007a2bb61ce76549b
Daniel Miranda <Dm2639532@gmail.com>
2023-09-18 14:54:53 -0300

    Criacao do projeto de cartao de visita virtual

---
+++ Assets/JS/main.js
@@ -0,0 +1,16 @@
+const LightOrDark = document.getElementById('Light-or-dark');
+const sunOrMoon = document.getElementById('sun-or-moon');
+const changeTheme = document.getElementById('swtich-theme')
+
+let change = 1;
+sunOrMoon.addEventListener('click', () => {
+    if(change == 1){
+        changeTheme.href = "./Assets/CSS/dark-mode.css";
+        sunOrMoon.src = "./Assets/IMG/sunny.svg";
+        change -= 1;
+    } else {
+        changeTheme.href = "./Assets/CSS/style.css";
+        sunOrMoon.src = "./Assets/IMG/moon.svg";
+        change += 1;
+    }
+})
